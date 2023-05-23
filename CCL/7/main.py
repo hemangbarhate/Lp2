fb64f418f2845a8aa5064436872c9fc0f7ba98fc
hemangbarhate <hemangbarhate@gmail.com>
2023-05-23 09:35:37 +0530

    commit

--- CCL/7/main.py
+++ CCL/7/main.py
@@ -1,28 +1,32 @@
 import webapp2
-import urllib
 import json
+import urllib
 from google.appengine.ext.webapp import template
 
-class MainPage(webapp2.RequestHandler):
+class HomePage(webapp2.RequestHandler):
     def get(self):
         temp = {}
-        self.response.write(template.render("./template/index.html", temp))
+        self.response.write(template.render('./template/index.html',temp))
 
     def post(self):
         pincode = self.request.get('pincode')
         url = "https://api.postalpincode.in/pincode/" + pincode
-        data = urllib.urlopen(url).read() #Requested
-        data = json.loads(data) #data converted to json(dict)
-        status = data[0]['Status']
+
+        data = urllib.urlopen(url).read()
+        data = json.loads(data)
+
+        status = data[0]["Status"]
 
         if status == "Success":
-            name = data[0]['PostOffice'][0]['Name']
-            region = data[0]['PostOffice'][0]['Region']
-            temp = {"name": name, "region": region}
-            self.response.write(template.render('./template/results.html', temp))
-            
-        if status == "Error":
-            temp = {"data": data}
-            self.response.write(template.render('./template/error.html', temp))
-       
-app = webapp2.WSGIApplication([('/', MainPage)])
+            name = data[0]["PostOffice"][0]["Name"]
+            region = data[0]["PostOffice"][0]["Name"]
+
+            temp = {"name":name,"region":region}
+            self.response.write(template.render('./template/results.html',temp))
+        else :
+            temp = {"data":data}
+            self.response.write(template.render('./template/results.html',temp))
+
+
+
+app = webapp2.WSGIApplication([("/",HomePage)],debug=True)
